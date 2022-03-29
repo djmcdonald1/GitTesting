06d1f693a270fadda57f407bacdef6159b1f165b
djmcdonald1 <35308171+djmcdonald1@users.noreply.github.com>
2022-03-28 20:19:01 -0700

    Initial Commit

---
+++ GitTesting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GitTesting
+//
+//  Created by Dan McDonald on 3/28/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
